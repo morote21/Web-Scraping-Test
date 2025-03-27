0c4c6f0d9730a5a2d87c97af2db51c8735e694c0
morote21 <josemg00021@gmail.com>
2025-03-27 01:32:33 +0100

    init working fine

--- source/nba_test/nba_test.py
+++ source/nba_test/nba_test.py
@@ -6,32 +6,29 @@ from urllib import robotparser
 
 class NBAScraper():
 
-    def __init__(self, user_agent, webdriver_path):
+    def __init__(self, user_agent):
         self.rp = robotparser.RobotFileParser()
         self.user_agent = user_agent
 
         # https://developer.chrome.com/docs/chromedriver/capabilities?hl=es-419
         webdriver_options = ChromeOptions()
-        webdriver_options.binary_location = webdriver_path
         # webdriver_options.add_argument("--headless")
-        webdriver_options.add_argument(str("--user-agent=" + user_agent))
+        webdriver_options.add_argument(f"--user-agent={user_agent}")
+        self.driver = webdriver.Chrome(options=webdriver_options)
         
-        webdriver_service = ChromeService(executable_path=webdriver_path)
-        print(webdriver_options.arguments)
-
-        self.driver = webdriver.Chrome()
 
     # Comprobar accesibilidad al sitio web
     def check_accessibility(self, url) -> bool:
         return self.rp.can_fetch(useragent=self.user_agent, url=url)
+    
 
     def quit_driver(self):
         self.driver.quit()
 
 
 def main():
-    user_agent_linux = "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0"
-    scraper = NBAScraper(user_agent=user_agent_linux, webdriver_path="chromedriver.exe")
+    user_agent_windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+    scraper = NBAScraper(user_agent=user_agent_windows)
     print(scraper.check_accessibility("https://www.nba.com/stats"))
 
     scraper.quit_driver()
